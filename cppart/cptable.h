cc213b7fb99eb5488898a40366082fb3792523cc
Anthony Morast <anthonymorast33@hotmail.com>
2018-05-14 16:12:28 -0600

    removed old files from git

--- cppart/cptable.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <vector>
-
-#include "structures.h"
-
-void makeCpList(node *n, double &parentCp, vector<double> &cpList, int &uniqueCp);
-cpTable* makeCpTable(node *n, double parentCp, int nSplit, cpTable *cpTail);
