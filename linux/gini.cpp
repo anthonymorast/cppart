17dd893e1db820c04eaeda2aa569e3b43e8bfe20
Anthony Morast <anthonymorast33@hotmail.com>
2018-06-19 23:53:45 -0600

    gini split is working, not building the right trees

--- linux/gini.cpp
+++ linux/gini.cpp
@@ -27,15 +27,20 @@ void giniDev(float y[], int numValues, double &classification, double &deviance)
         }
     }
 
-    // get risk (stolen again from rpart)
+    // get risk (stolen again from rpart, for the most part)
+    // loss = hollow matrix with 1's upper and lower triangles of size numclass x numclass
     deviance = 0;
     int numclasses = classFreq.size();
-    for(int i = 0; i < numclasses; i++) {
+    for (int i = 0; i < numclasses; i++) {
         int temp = 0;
-        for(int j = 0; j < numclasses; j++) {
-            temp += classFreq.at(j);
+        int j = 0;
+        for(auto it = classFreq.cbegin(); it != classFreq.cend(); it++) {
+            int loss_mult = j == i ? 0 : 1; // on diagonal = 0, otherwise 1
+            temp += (it->second * loss_mult);
+            j++;
         }
         if(i == 0 || temp < deviance) {
+            max = i;
             deviance = temp;
         }
     }
@@ -48,48 +53,53 @@ void giniSplit(float *x, float *y, params *p, int &which,
     int i, j, k;
     double lwt = 0, rwt = 0;
     int rtot = 0, ltot = 0;
-    direction = LEFT; // which = where
+    direction = LEFT; 
+    which = 0; // which = where
     double total_ss, best, temp, pp;
     double lmean, rmean;
 
-    double *left = new double[p->numclasses], *right = new double[p->numclasses]; 
-
-    for(i = 0; i < p->numclasses; i++) {
-        left[i] = 0;
-        right[i] = 0;
+    // initialize count of class instances left and right of split (all start in right)
+    map<float, int> rightMap, leftMap;
+    for(int i = 0; i < n; i++) {
+        if(rightMap.count(y[i])) { // if == 1, key is in map
+        } else {
+            rightMap.insert(pair<float, int>(y[i], 0));
+            leftMap.insert(pair<float, int>(y[i], 0));
+        }
     }
 
-    for(i = 0; i < i < n; i++) {
-        j = (int) y[i] - 1;
+    for(i = 0; i < n; i++) {
         rwt++; // += aprior[j] * wt[i] => 1*1
-        right[j]++; // += wt[i]
+        rightMap.at(y[i])++; // += wt[i] 
         rtot++;
     }
 
     total_ss = 0;
-    for(i = 0; i < p->numclasses; i++) {
-        temp = 1 * right[i] / rwt; // = aprior[i] * right[i] / rwt
+    for(auto it = rightMap.cbegin(); it != rightMap.cend(); it++) {
+        temp = 1 * it->second / rwt;
         total_ss += rwt * impure(temp);
     }
     best = total_ss;
 
     for(i = 0; rtot > p->minNode; i++) {
-        j = (int) y[i] - 1;
+        j = y[i];
         rwt--; // -= aprior[j] * wt[i]
         lwt++; // += aprior[j] * wt[i]
         rtot--;
         ltot++;
-        right[j]--; // -= wt[i]
-        left[j]++; // += wt[i]
+        rightMap.at(j)--; // -= wt[i]
+        leftMap.at(j)++; // += wt[i]
         if (ltot >= p->minNode && x[i+1] != x[i]) {
             temp = 0;
             lmean = 0;
             rmean = 0;
-            for(j = 0; j < p->numclasses; j++) {
-                pp = 1 * left[j] / lwt; // aprior[j] * left[j] / lwt
+            for(auto right = rightMap.cbegin(), left = leftMap.cbegin(); 
+                    right != rightMap.cend() && left != leftMap.cend();  right++, left++) {
+                // key order should always be the same for std::map structures
+                pp = 1 * left->second / lwt; // aprior[j] * left[j] / lwt
                 temp += lwt * impure(pp);
                 lmean += pp * j;
-                pp = 1 * right[j] / rwt; // aprior[j] * right[j] / rwt
+                pp = 1 * right->second / rwt; // aprior[j] * right[j] / rwt
                 temp += rwt * impure(pp);
                 rmean += pp * j;
             }
@@ -103,9 +113,4 @@ void giniSplit(float *x, float *y, params *p, int &which,
 
     improve = total_ss - best;
     splitPoint = (n > which + 1) ? ((x[which] + x[which + 1]) / 2) : x[which];
-
-    delete[] left;
-    delete[] right;
-    right = NULL;
-    left = NULL; 
 }
