f6ae9a1bb5d58c9ec6383cc9674981946199b419
Anthony Morast <anthonymorast33@hotmail.com>
2018-11-18 15:43:46 -0700

    made some headway on getting predictions from lacart

--- pyeatt/lacart.cc
+++ pyeatt/lacart.cc
@@ -58,13 +58,6 @@ int main(int argc, char* argv[]) {
 	// create data table for training
 
 	DataTable *Data = new DataTable(p.varNames,p.trainData,p.trainSize,getColumnCount(p.headers));
-
-	// if(p.verbose>1)    // training data table:
-	//   {
-	// 	cout<<"training data:"<<endl;
-	// 	Data->dump();
-	//   }
-
 	start = clock();
 
 	//int numNodes = 0;
@@ -107,44 +100,35 @@ int main(int argc, char* argv[]) {
 	//     cpTableFilename += ".delayed";
 	// }
 	// cpTableFilename += ".cptable";
+	// printCpTable(cpTableHead, cpTableFilename);
 
-
-
-	// //    printCpTable(cpTableHead, cpTableFilename);
-
-	// cout << endl << endl << "Results: " << endl;
-	// uncommented because warnings suck
+	cout << endl << endl << "Results: " << endl;
 	cout << "Time elapsed " << ((double)(end - start)) / CLOCKS_PER_SEC << endl;
-	// cout << "Test Data: " << endl;
-	// double mae = 0;
-	// double relError = 0;
-	// int correct = 0, incorrect = 0;
+	cout << "Test Data: " << endl;
+	double mae = 0;
+	double relError = 0;
+	int correct = 0, incorrect = 0;
 
-
-
-	// for (int i = 0; i < p.testSize; i++) {
-	//     double *sample = p.testData[i];
-	//     int respCol = getResponseColumnNumber(p.response, p.headers);
-	//     double pred = getPrediction(&root, sample, respCol);
-	//     double e = abs(pred - sample[respCol]);
-	//     if(pred == sample[respCol]) {
-	//         correct++;
-	//     } else {
-	//         incorrect++;
-	//     }
-	//     relError += e/1+abs(sample[respCol]); // class 0 = nan, need to add 1 to denominator
-	//     mae += e;
-	//     // error += anovaPredict(sample[0], pred); // square error
-	//     //cout << "Actual: " << sample[respCol] << "\tPredicted: " <<  pred << endl;
-	// }
-	// if(p.method == ANOVA) {
-	//     cout << "\tMAE: " << mae/p.testSize << endl;
-	// } else {
-	//     cout << "\tCorrect Classifications: " << correct << endl;
-	//     cout << "\tIncorrect Classifications: " << incorrect << endl;
-	//     cout << "\tClassification Error (correct/total): " << (double)correct/p.testSize << endl;
-	// }
-	// cout << "\tRelative Error: " << relError/p.testSize << endl << endl;
+	for (int i = 0; i < p.testSize; i++) {
+	    double *sample = p.testData[i];
+	    double pred = tree->predict(sample);
+	    double e = abs(pred - sample[0]);
+	    if(pred == sample[respCol]) {
+	        correct++;
+	    } else {
+	        incorrect++;
+	    }
+	    relError += e/1+abs(sample[respCol]); // class 0 = nan, need to add 1 to denominator
+	    mae += e;
+	}
+	if(p.method == ANOVA) {
+	    cout << "\tMAE: " << mae/p.testSize << endl;
+	} else {
+	    cout << "\tCorrect Classifications: " << correct << endl;
+	    cout << "\tIncorrect Classifications: " << incorrect << endl;
+	    cout << "\tClassification Error (correct/total): " << (double)correct/p.testSize << endl;
+	}
+	cout << "\tRelative Error: " << relError/p.testSize << endl << endl;
 
 	return 0;
 }
