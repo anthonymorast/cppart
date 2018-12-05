6167df759f9cce5546e46e5710ef1545be4c7a11
Anthony Morast <anthonymorast33@hotmail.com>
2018-12-04 22:00:51 -0700

    updated make multi script to use output format of the cart alg.

--- data_utils/makeMultiCsv.py
+++ data_utils/makeMultiCsv.py
@@ -7,13 +7,18 @@ import sys
 import csv
 
 def printUsage():
-    print("python3 makeMultiCsv.py <dataset_name>")
+    print("python3 makeMultiCsv.py <dataset_name> [optional: {big, small}")
+    print("big/small - depths to use big=[6, 8, 10, 12, 14, 16, 18] small=[1, 2, 3, 4, 5, 6]")
 
 
 if __name__ == '__main__':
     if(len(sys.argv) < 2):
         printUsage()
         exit(0)
+
+    depths_to_use = 'big'
+    if len(sys.argv) == 3:
+        depths_to_use = 'small'
 
     name = sys.argv[1]
     delays = [0,1]
@@ -24,10 +29,10 @@ if __name__ == '__main__':
 
     f = open(name+'Multi.csv', 'w', newline='')
     writer = csv.writer(f, delimiter=separator, quotechar='', quoting=csv.QUOTE_NONE)
+    writer.writerow(headers)
     for d in delays:
-        writer.writerow(headers)
         for depth in depths:
             for i in range(5):
-                row = [name, d, depth, '']
+                row = [name, '']
                 writer.writerow(row)
     f.close()
