6dec4be03f38afbf7efa0c1c1a7fd3bfc11f0296
Anthony Morast <anthonymorast33@hotmail.com>
2018-12-12 17:59:08 -0700

    more results, fixed multi processing script

--- data_utils/makeMultiCsv.py
+++ data_utils/makeMultiCsv.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
 
     depths_to_use = 'big'
     if len(sys.argv) == 3:
-        depths_to_use = 'small'
+        depths_to_use = sys.argv[2]
 
     name = sys.argv[1]
     delays = [0,1]
@@ -27,6 +27,8 @@ if __name__ == '__main__':
         depths = [1, 2, 3, 4, 5, 6] 
     headers = ['dataset','delayed','depth','impurity','relerror','accuracy']
     separator = ','
+
+    print(depths)
 
     f = open(name+'Multi.csv', 'w', newline='')
     writer = csv.writer(f, delimiter=separator, quotechar='', quoting=csv.QUOTE_NONE)
